4fa557a3b00f48a77815c7e95da8129ec05b9440
TBjustice <56857367+TBjustice@users.noreply.github.com>
2021-10-09 13:09:34 +0900

    Create turtle1.py

---
+++ Play with Turtle/turtle1.py
@@ -0,0 +1,14 @@
+from turtle import *
+import time
+Pen()
+speed(1)
+forward(100)
+left(90)
+forward(100)
+time.sleep(3)
+reset()
+speed(10)
+for i in range(60):
+    forward(200)
+    left(114)
+    
